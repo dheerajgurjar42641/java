9f7788ead50791cb4068b22f8f3f65af48189fb2
dheerajgurjar42641 <dheerajgurjar42641>
2026-06-12 10:13:57 +0530

    with_string

---
+++ Stack2.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+public class Stack2 {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("ramu");
+        stack.push("shamu");
+        stack.push("santosh");
+        stack.push("ashu");
+        System.out.println(stack);
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.pop());
+        System.out.println(stack);
+
+    }
+}
